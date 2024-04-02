d07a95fe34a62dc4a721c043c193301a448ec254
Caitlin Campbell <cac226@case.edu>
2024-04-02 15:49:16 -0500

    Initial commit

---
+++ WordleVariations/GuessResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordleVariations
+{
+    internal enum GuessResult
+    {
+        CORRECT = 0,
+        INCORRECT = 1,
+        RIGHT_LETTER_WRONG_LOCATION = 2,
+    }
+}
---
+++ WordleVariations/SecretWord.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordleVariations
+{
+    internal class SecretWord
+    {
+        private readonly string secretWord; 
+
+        public SecretWord(string word)
+        {
+            secretWord = word;
+        }
+
+        public GuessResult[] GuessWord(string guess)
+        {
+            GuessResult[] result = new GuessResult[guess.Length];
+
+            for(int i = 0; i < guess.Length; i++)
+            {
+                if (guess[i] == secretWord[i])
+                {
+                    result[i] = GuessResult.CORRECT;
+                } else if (!containsLetter(guess[i]))
+                {
+                    result[i] = GuessResult.INCORRECT;
+                } else
+                {
+                    result[i] = GuessResult.RIGHT_LETTER_WRONG_LOCATION;
+                }
+            }
+
+            return result;
+        }
+
+        private bool containsLetter(char c)
+        {
+            return secretWord.Contains(c);
+        }
+    }
+}
